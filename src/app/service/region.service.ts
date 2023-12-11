ca3691c3f4e6966745891fba48b204b35bf8562e
solomon141 <weymit2001@gmail.com>
2023-12-11 16:45:48 +0300

    All FIles

--- src/app/service/region.service.ts
+++ src/app/service/region.service.ts
@@ -1,14 +1,31 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Region } from '../model/region';
+
 @Injectable({
   providedIn: 'root',
 })
 export class RegionService {
   constructor(private httpClient: HttpClient) {}
 
+  private allRegions: Region[] = [
+    {
+        "regionId": 1,
+        "name": "Addis Ababa"
+    },
+    {
+        "regionId": 2,
+        "name": "Mekelle"
+    },
+    {
+        "regionId": 3,
+        "name": "Jimma"
+    }
+];
+
   fetchRegions(): Observable<any> {
-    const url = 'http://localhost:8083/region';
-    return this.httpClient.get(url);
+    return this.httpClient.get('http://localhost:8083/region');
+    // return of(this.allRegions);
   }
 }
--- src/app/service/rooms.service.ts
+++ src/app/service/rooms.service.ts
@@ -7,158 +7,155 @@ import { Rooms } from '../model/rooms';
 @Injectable({
   providedIn: 'root',
 })
-
-
-
 export class RoomsService {
   constructor(private httpClient: HttpClient) {}
 
   private allRooms: Rooms[] = [
-  {
-      "roomId": 4,
-      "imgPath": "assets/search/1.jpg",
-      "area": "new area",
-      "price": "100000",
-      "descr": null,
-      "isCompleted": false,
-      "floor": {
-          "floorId": 1,
-          "floorName": "First Floor"
+    {
+      roomId: 4,
+      imgPath: 'assets/search/1.jpg',
+      area: 'new area',
+      price: '100000',
+      descr: null,
+      isCompleted: false,
+      floor: {
+        floorId: 1,
+        floorName: 'First Floor',
       },
-      "region": {
-          "regionId": 1,
-          "name": "Addis Ababa"
+      region: {
+        regionId: 1,
+        name: 'Addis Ababa',
       },
-      "payment": {
-          "pmId": 1,
-          "pmName": "Long Term"
+      payment: {
+        pmId: 1,
+        pmName: 'Long Term',
       },
-      "roomSize": {
-          "roomSizeId": 1,
-          "name": "1 Bed Room"
-      }
-  },
-  {
-      "roomId": 5,
-      "imgPath": "assets/search/2.jpg",
-      "area": "new area",
-      "price": "100000",
-      "descr": null,
-      "isCompleted": false,
-      "floor": {
-          "floorId": 2,
-          "floorName": "Second Floor"
+      roomSize: {
+        roomSizeId: 1,
+        name: '1 Bed Room',
       },
-      "region": {
-          "regionId": 1,
-          "name": "Addis Ababa"
+    },
+    {
+      roomId: 5,
+      imgPath: 'assets/search/2.jpg',
+      area: 'new area',
+      price: '100000',
+      descr: null,
+      isCompleted: false,
+      floor: {
+        floorId: 2,
+        floorName: 'Second Floor',
       },
-      "payment": {
-          "pmId": 1,
-          "pmName": "Long Term"
+      region: {
+        regionId: 1,
+        name: 'Addis Ababa',
       },
-      "roomSize": {
-          "roomSizeId": 2,
-          "name": "2 Bed Room"
-      }
-  },
-  {
-      "roomId": 6,
-      "imgPath": "assets/search/3.jpeg",
-      "area": "new area",
-      "price": "200000",
-      "descr": null,
-      "isCompleted": false,
-      "floor": {
-          "floorId": 2,
-          "floorName": "Second Floor"
+      payment: {
+        pmId: 1,
+        pmName: 'Long Term',
       },
-      "region": {
-          "regionId": 1,
-          "name": "Addis Ababa"
+      roomSize: {
+        roomSizeId: 2,
+        name: '2 Bed Room',
       },
-      "payment": {
-          "pmId": 2,
-          "pmName": "Short Term"
+    },
+    {
+      roomId: 6,
+      imgPath: 'assets/search/3.jpeg',
+      area: 'new area',
+      price: '200000',
+      descr: null,
+      isCompleted: false,
+      floor: {
+        floorId: 2,
+        floorName: 'Second Floor',
       },
-      "roomSize": {
-          "roomSizeId": 2,
-          "name": "2 Bed Room"
-      }
-  },
-  {
-      "roomId": 7,
-      "imgPath": "assets/search/6.webp",
-      "area": "new area",
-      "price": "100000",
-      "descr": null,
-      "isCompleted": false,
-      "floor": {
-          "floorId": 3,
-          "floorName": "Third Floor"
+      region: {
+        regionId: 1,
+        name: 'Addis Ababa',
       },
-      "region": {
-          "regionId": 1,
-          "name": "Addis Ababa"
+      payment: {
+        pmId: 2,
+        pmName: 'Short Term',
       },
-      "payment": {
-          "pmId": 2,
-          "pmName": "Short Term"
+      roomSize: {
+        roomSizeId: 2,
+        name: '2 Bed Room',
       },
-      "roomSize": {
-          "roomSizeId": 3,
-          "name": "3 Bed Room"
-      }
-  },
-  {
-      "roomId": 8,
-      "imgPath": "assets/search/7.jpg",
-      "area": "new area",
-      "price": "100000",
-      "descr": null,
-      "isCompleted": false,
-      "floor": {
-          "floorId": 3,
-          "floorName": "Third Floor"
+    },
+    {
+      roomId: 7,
+      imgPath: 'assets/search/6.webp',
+      area: 'new area',
+      price: '100000',
+      descr: null,
+      isCompleted: false,
+      floor: {
+        floorId: 3,
+        floorName: 'Third Floor',
       },
-      "region": {
-          "regionId": 1,
-          "name": "Addis Ababa"
+      region: {
+        regionId: 1,
+        name: 'Addis Ababa',
       },
-      "payment": {
-          "pmId": 2,
-          "pmName": "Short Term"
+      payment: {
+        pmId: 2,
+        pmName: 'Short Term',
       },
-      "roomSize": {
-          "roomSizeId": 3,
-          "name": "3 Bed Room"
-      }
-  },
-  {
-      "roomId": 9,
-      "imgPath": "assets/search/8.jpeg",
-      "area": "new area",
-      "price": "200000",
-      "descr": null,
-      "isCompleted": false,
-      "floor": {
-          "floorId": 3,
-          "floorName": "Third Floor"
+      roomSize: {
+        roomSizeId: 3,
+        name: '3 Bed Room',
       },
-      "region": {
-          "regionId": 1,
-          "name": "Addis Ababa"
+    },
+    {
+      roomId: 8,
+      imgPath: 'assets/search/7.jpg',
+      area: 'new area',
+      price: '100000',
+      descr: null,
+      isCompleted: false,
+      floor: {
+        floorId: 3,
+        floorName: 'Third Floor',
       },
-      "payment": {
-          "pmId": 1,
-          "pmName": "Long Term"
+      region: {
+        regionId: 1,
+        name: 'Addis Ababa',
       },
-      "roomSize": {
-          "roomSizeId": 3,
-          "name": "3 Bed Room"
-      }
-  }
-];
+      payment: {
+        pmId: 2,
+        pmName: 'Short Term',
+      },
+      roomSize: {
+        roomSizeId: 3,
+        name: '3 Bed Room',
+      },
+    },
+    {
+      roomId: 9,
+      imgPath: 'assets/search/8.jpeg',
+      area: 'new area',
+      price: '200000',
+      descr: null,
+      isCompleted: false,
+      floor: {
+        floorId: 3,
+        floorName: 'Third Floor',
+      },
+      region: {
+        regionId: 1,
+        name: 'Addis Ababa',
+      },
+      payment: {
+        pmId: 1,
+        pmName: 'Long Term',
+      },
+      roomSize: {
+        roomSizeId: 3,
+        name: '3 Bed Room',
+      },
+    },
+  ];
 
   getRooms(): Observable<Rooms[]> {
     console.log('geting rooms');
@@ -167,7 +164,8 @@ export class RoomsService {
   }
 
   getRoomsWithFilterParams(roomId: number): Observable<Rooms[]> {
-    return this.httpClient.get<Rooms[]>(`http://localhost:8083/rooms/`+ roomId);
+    return this.httpClient.get<Rooms[]>(
+      `http://localhost:8083/rooms/` + roomId
+    );
   }
-
 }
