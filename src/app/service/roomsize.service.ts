5eb4de7607e973e6222225882200ca27068e783d
solomon141 <weymit2001@gmail.com>
2023-12-11 17:16:34 +0300

    fake api edited roomsize included

--- src/app/service/roomsize.service.ts
+++ src/app/service/roomsize.service.ts
@@ -3,16 +3,31 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Roomsize } from '../model/roomsize';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RoomsizeService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
+  private allRoomsize: Roomsize[] = [
+    {
+      roomSizeId: 1,
+      name: '1 Bed Room',
+    },
+    {
+      roomSizeId: 2,
+      name: '2 Bed Room',
+    },
+    {
+      roomSizeId: 3,
+      name: '3 Bed Room',
+    },
+  ];
 
   getRoomSizes(): Observable<Roomsize[]> {
-    return this.httpClient.get<Roomsize[]>(`http://localhost:8083/roomsize`);
+    // return this.httpClient.get<Roomsize[]>(`http://localhost:8083/roomsize`);
+    return of(this.allRoomsize);
   }
-
 }
